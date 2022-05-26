6d1a5d5345b1661ecd62f98101e78bd4d5542a15
Julien Achkar <julien_achkar@outlook.com>
2022-05-26 17:26:28 +0300

    Window Display

--- PythonTest.py
+++ PythonTest.py
@@ -1,6 +1,9 @@
-from tkinter import *
-window = Tk()
+import tkinter
 
+window = tkinter.Tk()
+window.title("Python GUI App")
 
+window.minsize(500, 300)
 
-
+# Calling mainloop() allows tkinter to run the application
+window.mainloop()
